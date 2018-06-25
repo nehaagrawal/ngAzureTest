e029a53452424e6a1f2f97d30ca1b3df324a7730
nehaagrawal <nehaagrawal30@gmail.com>
2018-06-24 22:18:07 -0700

    initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,30 @@
+import { Routes } from '@angular/router';
+import { HomeComponent} from './home.component'
+import { ProductComponent} from './product.component'
+import { ErrorComponent} from './error.component'
+import { ProductDetailComponent} from './product-detail.component';
+import {MsalGuard} from "ms-msal-angular";
+import {MsGraphComponent} from "./msGraph.component";
+import {TodoListComponent} from "./todo-list/todo-list.component";
+import {UserDataComponent} from "./user-data/user-data.component";
+import {ConfigComponent} from "./config/config.component";
+
+export const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent  },
+  { path: 'todoList', component: TodoListComponent , canActivate : [MsalGuard] },
+  { path: 'product', component: ProductComponent,canActivate : [MsalGuard],
+    children: [
+      { path: 'detail/:id', component: ProductDetailComponent  }
+    ]
+   },
+  { path: 'myCalendar' ,component: MsGraphComponent, canActivate : [MsalGuard]},
+  { path: 'userData' ,component: UserDataComponent},
+  { path: 'config' ,component: ConfigComponent},
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: ErrorComponent }
+];
+
+
+
+
