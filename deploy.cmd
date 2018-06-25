1850ccfd796b7aaf3eab9b740c1125570f065f3e
nehaagrawal <nehaagrawal30@gmail.com>
2018-06-25 15:36:35 -0700

    updated config

--- deploy.cmd
+++ deploy.cmd
@@ -110,6 +110,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 ::)
 
 
+
 :: 1. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
